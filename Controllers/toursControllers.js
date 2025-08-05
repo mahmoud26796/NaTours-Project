05503feced510298e6c45b4b81beadea4dd6888b
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-08-05 21:48:43 +0300

    added some code for aggregation pipleines with some new routes

--- Controllers/toursControllers.js
+++ Controllers/toursControllers.js
@@ -22,6 +22,41 @@ const APIFeatures = require('../utils/apiFeatures.js');
     }
 };
 
+exports.getToursStats = async (req, res) => {
+    try {
+            const stats = await Tour.aggregate([
+                {
+                    $match: { ratingsAverage: { $gte: 4.5 } }
+                },
+                {
+                    $group: {
+                        _id: '$difficulty',
+                        numTours: {$sum: 1},
+                        numRatings : {$sum: '$ratingsQuantity'},
+                        avgRating: {$avg: '$ratingsAverage'},
+                        avgPrice: {$avg: '$price'},
+                        minPrice: {$min: '$price'},
+                        maxPrice: {$max: '$price'}
+                    },
+
+                },
+                {
+                  $sort: { avgPrice : 1 }
+                }
+            ]);
+        res.status(200).json({
+         status: 'success',
+         data: {
+            stats
+          }
+      });
+    } catch (error) {
+        res.status(404).json({
+         status: 'Fail',
+         message: "Document Not Found!"
+      });
+    }
+};
 exports.getTourByID = async (req, res) => {
     const tour = await Tour.findById(req.params.id);
     try {
@@ -90,6 +125,48 @@ exports.removeTour = async (req, res) => {
             status: "Fail",
             result: {
                 message: "Tour Not Found, Deletion disapproved"
+            }
+        });
+    }
+};
+
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1;
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates'
+            },
+            {
+                $match : {
+                    startDates : {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`)
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numToursStart: { $sum : 1 }
+                }
+            },
+        ]);
+
+        res.status(200).json({
+            status: "Success",
+            result: plan.length,
+            data: {
+              plan
+           }
+        });
+        
+    } catch (error) {
+        res.status(404).json({
+            status: "Fail",
+            result: {
+                message: "Data Not Found"
             }
         });
     }
--- routes/toursRouter.js
+++ routes/toursRouter.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
-const {getAllTours, getTourByID,addNewTour, updateTour, removeTour} = require('../Controllers/toursControllers');
+const {getAllTours, getTourByID,addNewTour, updateTour, removeTour, getToursStats, getMonthlyPlan} = require('../Controllers/toursControllers');
 
 // tours routes
 router
     .route('/').get(getAllTours)
     .post(addNewTour);
+router
+    .route('/tours-stats').get(getToursStats);
+router
+    .route('/monthly-plan/:year').get(getMonthlyPlan);
 router
     .route('/:id')
     .get(getTourByID)
