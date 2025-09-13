7eef703aa4e888b57597605c1f14850e531e0637
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-09-13 20:03:15 +0300

    fixed a bug of passing to populate path and object while it should be string

--- models/tourModel.js
+++ models/tourModel.js
@@ -130,7 +130,7 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.pre(/^find/, function (next) {
   this.populate({
-    path: guides,
+    path: 'guides',
     select: "-__v -passwordChangedAt",
   });
   next();
