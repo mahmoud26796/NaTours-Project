613cbe1a8561eed13caf24435765c2ac5678c315
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-09-28 17:13:03 +0300

    passing tours to template overview

--- Controllers/viewsControllers.js
+++ Controllers/viewsControllers.js
@@ -1,16 +1,14 @@
-exports.renderRoot = (req, res) => {
-  res.status(200).render("base", {
-    user: {
-      name: "jonas",
-    },
-  });
-};
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
 
-exports.renderAllTours = (req, res) => {
-  res.status(200).render("overview", {
-    data: "All Tours",
-  });
-};
+exports.renderRoot = catchAsync(async (req, res) => {
+  res.status(200).render("base");
+});
+
+exports.renderAllTours = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
+  res.status(200).render("overview", { tours });
+});
 
 exports.renderTour = (req, res) => {
   res.status(200).render("tour", {
