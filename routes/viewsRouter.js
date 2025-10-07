4c791fa369df1736fbe6f62add739961c81bd8fb
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-10-07 22:01:15 +0300

    re-order routers middleware so the root page can render the logged in user infos

--- routes/viewsRouter.js
+++ routes/viewsRouter.js
@@ -7,11 +7,9 @@ const {
 const { protect, isLoggedIn } = require("../Controllers/authControllers");
 const router = express.Router();
 
-router.get("/", renderAllTours);
-
 router.get("/login", renderLoginPage);
-
 router.use(isLoggedIn);
+router.get("/", renderAllTours);
 router.get("/tour/:slug", renderTour);
 
 module.exports = router;
