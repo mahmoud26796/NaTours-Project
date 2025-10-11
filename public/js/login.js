183ec0f7fe6970b4054933b390dc674e3deb8995
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-10-11 23:22:34 +0300

    moved login logic to this file

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 // const axios = require("axios");
-const login = async (email, password) => {
+import axios from "axios";
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: "POST",
@@ -9,7 +10,7 @@ const login = async (email, password) => {
         password,
       },
     });
-    console.log(res.data)
+    console.log(res.data);
     if (res.data.status === "success") {
       alert("Your Are Logged In Suceessfuly!");
       window.setTimeout(() => {
@@ -20,10 +21,3 @@ const login = async (email, password) => {
     console.log(e.response.data);
   }
 };
-
-document.querySelector(".form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  login(email, password);
-});
